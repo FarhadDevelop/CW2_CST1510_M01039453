285f9facf43bf02771ccd6ee3192cf7a8a5e498c
Farhad Binu Manaf <FB569@live.mdx.ac.uk>
2025-11-14 15:03:17 +0400

    Refactor database connection in tickets.py to remove hardcoded DB path

--- app/data/tickets.py
+++ app/data/tickets.py
@@ -3,11 +3,8 @@ import pandas as pd
 from app.data.db import connect_database
 from pathlib import Path
 
-DATA_DIR = Path("CW2_CST1510_M01039453/DATA")
-DB_PATH = DATA_DIR / "intelligence_platform.db"
-
 def insert_ticket(priority, description, status, assigned_to, created_at, resolution_time_hours):
-    conn = connect_database(DB_PATH)
+    conn = connect_database()
     cursor = conn.cursor()
     cursor.execute("""
         INSERT INTO tickets (priority, description, status, assigned_to, created_at, resolution_time_hours)
@@ -17,13 +14,13 @@ def insert_ticket(priority, description, status, assigned_to, created_at, resolu
     conn.close()
 
 def get_all_tickets():
-    conn = connect_database(DB_PATH)
+    conn = connect_database()
     df = pd.read_sql_query("SELECT * FROM tickets", conn)
     conn.close()
     return df
 
 def update_ticket_status(ticket_id, new_status):
-    conn = connect_database(DB_PATH)
+    conn = connect_database()
     cursor = conn.cursor()
     cursor.execute("""
         UPDATE tickets
@@ -34,7 +31,7 @@ def update_ticket_status(ticket_id, new_status):
     conn.close()
 
 def delete_ticket(ticket_id):
-    conn = connect_database(DB_PATH)
+    conn = connect_database()
     cursor = conn.cursor()
     cursor.execute("DELETE FROM tickets WHERE ticket_id = ?", (ticket_id,))
     
