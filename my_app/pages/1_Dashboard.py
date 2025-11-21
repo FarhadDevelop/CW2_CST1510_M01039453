8735dd368e14d3037377a512c3be485c8fa0fbc1
Farhad Binu Manaf <FB569@live.mdx.ac.uk>
2025-11-21 20:38:51 +0400

    Fix page navigation in dashboard: update path to include project directory

--- my_app/pages/1_Dashboard.py
+++ my_app/pages/1_Dashboard.py
@@ -14,7 +14,7 @@ if "username" not in st.session_state:
 if not st.session_state.logged_in:
     st.error("You must be logged in to view the dashboard.")
     if st.button("Go to login page"):
-        st.switch_page("Home.py")      # back to the first page
+        st.switch_page("CW2_CST1510_M01039453/my_app/Home.py")      # back to the first page
     st.stop()
 
 # If logged in, show dashboard content
